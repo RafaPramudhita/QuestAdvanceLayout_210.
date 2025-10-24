9bf0e8eb4c804043261c6c2dadafe07b0cdd2a7c
RafaPramudhita <rafapram360@gmail.com>
2025-10-24 14:53:54 +0700

    1.fitur: menambahkan layout dasar Column dan teks header

--- app/src/main/java/com/example/myuipertama/UIPertama.kt
+++ app/src/main/java/com/example/myuipertama/UIPertama.kt
@@ -1,3 +1,30 @@
 package com.example.myuipertama
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
+@Composable
+fun ActivitasPertama(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier
+            .padding(top = 100.dp)
+            .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+    }
+}
